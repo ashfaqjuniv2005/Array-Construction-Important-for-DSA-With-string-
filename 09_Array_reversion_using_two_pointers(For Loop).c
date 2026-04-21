94f460d41e0c26883d06c7f449fdf15527db65f7
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2026-04-21 20:12:22 +0600

    Implement array reversal using two-pointer technique

---
+++ 09_Array_reversion_using_two_pointers(For Loop).c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <limits.h>
+#include <float.h>
+#include <assert.h>
+#include <errno.h>
+
+
+
+int main()
+{
+
+    int n;scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0,j=n-1;i<j;i++,j--)
+    {
+        int tmp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=tmp;
+    }
+
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;
+}
