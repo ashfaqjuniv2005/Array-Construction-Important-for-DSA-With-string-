5471497f104ae33faca1c93ff9fed92f100615d2
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2026-04-21 11:48:59 +0600

    Create 02_Insert_a_value_in_array.c

---
+++ 02_Insert_a_value_in_array.c
@@ -0,0 +1,38 @@
+// Inserting a value in array
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <limits.h>
+#include <float.h>
+#include <assert.h>
+#include <errno.h>
+
+
+
+int main()
+{
+         int n;scanf("%d",&n);
+            int arr[n+1];
+            for(int i=0;i<n;i++){
+                scanf("%d",&arr[i]);
+            }
+            
+            int idx,val;
+            scanf("%d%d",&idx,&val);
+            for(int i=n;i>idx;i--){
+                arr[i]=arr[i-1];
+            }
+            
+            arr[idx]=val;
+            printf("Array after insertion is:\n");
+            for(int i=0;i<=n;i++){
+                printf("%d ",arr[i]);
+            }
+  
+    return 0;
+}
